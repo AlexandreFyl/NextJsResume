741e1954c32a5d049d833a91a44bc06e45f8525b
AlexandreFyl <afayolle43@gmail.com>
2024-02-21 19:00:16 +0100

    basic intro section with glowin and gradient effects

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,8 +2,18 @@ import React from "react";
 
 const Header = () => {
   return (
-    <section className="h-screen flex justify-center items-center">
-      <h1>Hello World!</h1>
+    <section className="h-screen flex flex-col justify-center items-center">
+      <div>
+        <p className="text-glow text-7xl font-bold leading-tight">
+          Hello there,
+        </p>
+        <p className="text-glow text-5xl font-bold leading-tight">
+          I'm Fayolle Alexandre
+        </p>
+        <h1 className="text-gradient text-5xl font-bold leading-tight">
+          Full-Stack Developer
+        </h1>
+      </div>
     </section>
   );
 };
