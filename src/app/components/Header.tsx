be3758ef66b1088dd5d609fb32995339fa2b1c5a
AlexandreFyl <afayolle43@gmail.com>
2024-02-21 19:42:52 +0100

    responsive mobile first + fancy blurry bg

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,16 +3,18 @@ import React from "react";
 const Header = () => {
   return (
     <section className="h-screen flex flex-col justify-center items-center">
-      <div>
-        <p className="text-glow text-7xl font-bold leading-tight">
-          Hello there,
-        </p>
-        <p className="text-glow text-5xl font-bold leading-tight">
-          I'm Fayolle Alexandre
-        </p>
-        <h1 className="text-gradient text-5xl font-bold leading-tight">
-          Full-Stack Developer
-        </h1>
+      <div className="h-5/6 w-5/6 flex flex-col justify-center items-center blurry-bg-img">
+        <div>
+          <p className="text-glow text-5xl md:text-7xl font-bold leading-tight text-center md:text-left">
+            Hello there,
+          </p>
+          <p className="text-glow text-3xl md:text-5xl font-bold leading-tight text-center md:text-left">
+            I'm Fayolle Alexandre
+          </p>
+          <h1 className="text-gradient text-3xl md:text-4xl font-bold leading-tight text-center md:text-left">
+            Full-Stack Developer
+          </h1>
+        </div>
       </div>
     </section>
   );
