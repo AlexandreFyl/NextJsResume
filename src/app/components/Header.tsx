e91a34abe3503a3f6f5a279763b2584308c56e32
AlexandreFyl <afayolle43@gmail.com>
2024-02-21 18:05:48 +0100

    very simple hello world on external component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <section className="h-screen flex justify-center items-center">
+      <h1>Hello World!</h1>
+    </section>
+  );
+};
+
+export default Header;
